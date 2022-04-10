ab5b8b26c182fd39fde5caee85abfa1af25d4c55
monman53 <monman.cs@gmail.com>
2022-04-10 23:41:02 +0900

    Support public contents copy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::path::Path;
+
+extern crate fs_extra;
+
+fn main() -> Result<(), std::io::Error> {
+    // Clean-up `./dist` directory
+    if Path::new("./dist").exists() {
+        fs::remove_dir_all("./dist")?;
+        println!("`./dist` removed.");
+    }
+
+    // Create `./dist` directory
+    fs::create_dir("./dist")?;
+    println!("`./dist` created.");
+
+    // Copy all contents inside `./public` directory to `./dist`
+    if Path::new("./public").exists() {
+        // This option prevent creating `public` directory inside `./dist`
+        let mut options = fs_extra::dir::CopyOptions::new();
+        options.content_only = true;
+
+        let _ = fs_extra::dir::copy("./public/", "./dist", &options);
+        println!("Copied files inside `./public` into `./dist`.");
+    }
+
+    // Convert markups to html and place them inside `./dist`.
+    let paths = fs::read_dir("./markups").unwrap();
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display())
+    }
+
+    Ok(())
 }
